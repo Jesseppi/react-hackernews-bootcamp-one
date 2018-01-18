491bf760da1ebdf020a9fe8b6ce5675bdbfb9a65
Jesse Creech <jesse.creech@gmail.com>
2018-01-18 18:54:02 +1000

    chapter zero finished

--- source/index.js
+++ source/index.js
@@ -1,6 +1,27 @@
-  document.body.innerHTML =
-    `
-      <div>
-        Hello Hacker News
-      </div>
-    `;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var hackerString = "Hello Hacker News";
+var alertText = "Here is an alert";
+
+/* const element = React.createElement(
+  'div', // the type of element we're creating
+  null, // any props ('attributes') we're giving it
+  'Hello Hacker News' // The children of this element.
+); */
+
+const element = <div 
+style={{fontSize:36, color: 'green'}}
+onClick={() => window.alert(alertText)}
+>{hackerString}</div>;
+
+/* const number = 19;
+const element = React.createElement(
+  'div',
+  null,
+  number
+); */
+
+const root = document.getElementById('root');
+
+ReactDOM.render(element, root);
