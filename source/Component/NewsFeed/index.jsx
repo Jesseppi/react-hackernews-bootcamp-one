35c76c12603cf39a55c7ed32dc8ba6a31beafc64
Jesse Creech <jesse.creech@gmail.com>
2018-01-20 12:18:44 +1000

    chapter 3 done

---
+++ source/Component/NewsFeed/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDom from "react-dom";
+import styles from './styles.css';
+import Article from "../Article/";
+import HackerTitle from "../HackerTitle/";
+
+var hackerString = "Hello Hacker News";
+
+export default function NewsFeed(props){
+    return(
+        <div>
+        {props.articles.map(value => (
+            <Article key={value.index}
+                title ={value.title}
+                description = {value.description}
+                link = {value.link}
+            />      
+        )
+    )
+        }
+        </div>            
+    )
+  }
--- source/index.js
+++ source/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Article from "./Component/Article/";
-import HackerTitle from "./Component/HackerTitle/";
-
-var hackerString = "Hello Hacker News";
-var alertText = "Here is an alert";
+import NewsFeed from "./Component/NewsFeed/"
 
 var articleTitle = "FAKE NEWS ARTICLE";
 var articleDescription = 
@@ -13,31 +9,15 @@ var articleDescription =
 "LuLu is the free open-source macOS firewall that aims to block unknown outgoing connections, unless explicitly approved by the user.";
 var articleLink = "https://news.ycombinator.com/";
 
-function App (){
-  return(
-      <div>
-        <HackerTitle 
-          message={hackerString}
-          />
-        <Article
-          title ={articleTitle}
-          description = {articleDescription}
-          link = {articleLink}
-        />
-        <Article
-          title ={articleTitle}
-          description = {articleDescription}
-          link = {articleLink}
-        />
-        <Article
-          title ={articleTitle}
-          description = {articleDescription}
-          link = {articleLink}
-        />
-      </div>
-  )
-}
+var articles = [
+    {
+        title:articleTitle, 
+        description:articleDescription,
+        link:articleLink, 
+        index: 1
+    }
+];
 
 const root = document.getElementById('root');
 
-ReactDOM.render(<App/>, root);
+ReactDOM.render(<NewsFeed articles={articles}/>, root);
